277bbd088264a30df747f21ebac7d9ae2bc96784
Asadullo Akramov <asadullo.2807@gmail.com>
2022-03-05 00:39:01 +0500

    product list app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path:'', component:ProductListComponent},
+    ])  ,
     AppRoutingModule
   ],
   providers: [],
